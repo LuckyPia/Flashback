2ee8bae8773346fcb0ece728352d4aac0d91023b
664454335@qq.com <puyadong>
2021-11-02 11:40:20 +0800

    移除不必要文件

--- Example/Pods/Target Support Files/Flashback/Flashback-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Flashback : NSObject
-@end
-@implementation PodsDummy_Flashback
-@end
--- Example/Pods/Target Support Files/Pods-Flashback_Tests/Pods-Flashback_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Flashback_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Flashback_Tests
-@end
